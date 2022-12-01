c77bd1aa64886feaa1581514ef8cd982edc8cae3
Devin Spikowski <vegeta897@gmail.com>
2022-12-01 00:33:53 -0500

    ✨ Add day 1 solutions

---
+++ src/day01/README.md
@@ -0,0 +1,12 @@
+# 🎄 Advent of Code 2022 - day 1 🎄
+
+## Info
+
+Task description: [link](https://adventofcode.com/2022/day/1)
+
+## Notes
+
+Stumbles on the first day 😓
+
+1. Forgot that JS default array sorting treats numbers like strings!
+2. Forgot to add the last elf's calories to the list!
---
+++ src/day01/index.ts
@@ -0,0 +1,89 @@
+import run from 'aocrunner'
+
+const parseInput = (rawInput: string) =>
+	rawInput
+		.trim()
+		.split('\n')
+		.map((v) => +v)
+
+const getCaloriesPerElf = (input: (number | '')[]) => {
+	const calories = []
+	let current = 0
+	for (const line of input) {
+		if (line) {
+			current += line
+		} else {
+			calories.push(current)
+			current = 0
+		}
+	}
+	if (current) calories.push(current)
+	return calories
+}
+
+const part1 = (rawInput: string) => {
+	const input = parseInput(rawInput)
+	const calories = getCaloriesPerElf(input)
+	return calories.sort((a, b) => b - a)[0].toString()
+}
+
+const part2 = (rawInput: string) => {
+	const input = parseInput(rawInput)
+	const calories = getCaloriesPerElf(input)
+	return calories
+		.sort((a, b) => b - a)
+		.slice(0, 3)
+		.reduce((a, b) => a + b, 0)
+		.toString()
+}
+
+run({
+	part1: {
+		tests: [
+			{
+				input: `
+				1000
+				2000
+				3000
+				
+				4000
+				
+				5000
+				6000
+				
+				7000
+				8000
+				9000
+				
+				10000`,
+				expected: '24000',
+			},
+		],
+		solution: part1,
+	},
+	part2: {
+		tests: [
+			{
+				input: `
+				1000
+				2000
+				3000
+				
+				4000
+				
+				5000
+				6000
+				
+				7000
+				8000
+				9000
+				
+				10000`,
+				expected: '45000',
+			},
+		],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	// onlyTests: true,
+})
